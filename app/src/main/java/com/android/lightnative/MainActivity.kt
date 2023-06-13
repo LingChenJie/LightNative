b6f7ac7237bfaa5fc086ceed125cc84602b12ffd
chen <976918915@qq.com>
2023-06-13 18:03:41 +0800

    feat:提交代码

--- app/src/main/java/com/android/lightnative/MainActivity.kt
+++ app/src/main/java/com/android/lightnative/MainActivity.kt
@@ -14,21 +14,7 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
     }
 
-    /**
-     * A native method that is implemented by the 'lightnative' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
 
-    companion object {
-        // Used to load the 'lightnative' library on application startup.
-        init {
-            System.loadLibrary("lightnative")
-        }
-    }
 }
